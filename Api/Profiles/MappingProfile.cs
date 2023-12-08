ad82add21716d3e165f3dc7f09d7b8dd055790fc
Ongax <jhoseppulido1129@gmail.com>
2023-12-07 21:59:07 -0500

    Database assets added

--- Api/Profiles/MappingProfile.cs
+++ Api/Profiles/MappingProfile.cs
@@ -6,16 +6,17 @@ namespace API.Profiles
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile (){
-            CreateMap<Comment,CommentDto>().ReverseMap();
-            CreateMap<Course,CourseDto>().ReverseMap();
-            CreateMap<Instructor,InstructorDto>()
-            .ForMember(dest=>dest.Name,Opt=>Opt.MapFrom(en=>en.User.Username))
-            .ReverseMap();
-            CreateMap<Qualification,QualificationDto>().ReverseMap();
-            CreateMap<Course,CourseWithEntities>()
-            .AfterMap((src, dest) => dest.Instructor.Name = src.Instructor.User.Username)
-            .ReverseMap();
+        public MappingProfile()
+        {
+            CreateMap<Comment, CommentDto>().ReverseMap();
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Instructor, InstructorDto>()
+                .ForMember(dest => dest.Name, Opt => Opt.MapFrom(en => en.User.Username))
+                .ReverseMap();
+            CreateMap<Qualification, QualificationDto>().ReverseMap();
+            CreateMap<Course, CourseWithEntities>()
+                .AfterMap((src, dest) => dest.Instructor.Name = src.Instructor.User.Username)
+                .ReverseMap();
         }
     }
 }
